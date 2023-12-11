91d36b233a8090a6fc65e81aaba0a72f56893068
Katerrina2023 <152924282+Katerrina2023@users.noreply.github.com>
2023-12-11 20:50:08 +0300

    Delete js directory

--- js/modules/burger.js
+++
@@ -1,6 +0,0 @@
-$('.burger span').on('click', function() {
-    $(this).toggleClass('active')
-    $('.menu').toggleClass('burger-active')
-    $('.navbar__btns').toggleClass('burger-active')
-    $('body').toggleClass('lock')
-})
