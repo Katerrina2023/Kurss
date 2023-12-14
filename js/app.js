65631fe391df51f0074db6e4dd29166e7e9ca769
Katerrina2023 <152924282+Katerrina2023@users.noreply.github.com>
2023-12-14 11:54:59 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+$(".tabs__title").click(function() {
+    $(this).parent().children().removeClass("tabs__title_active")
+    $(this).addClass("tabs__title_active")
+    let tabId = $(this).attr("data-tab")
+    $(this).parent().next().children().removeClass("tabs__body_active")
+    $(this).parent().next().children(`[data-tab=${tabId}]`).addClass("tabs__body_active")
+})
+
+$(".spoiler__header").click(function() {
+    $(this).parent().parent().children().removeClass("spoiler_active")
+    $(this).parent().addClass("spoiler_active")
+})
+
+$(".tag").click(function() {
+    $(this).toggleClass("tag_active")
+})
+
+$(".tags__header").click(function() {
+    $(this).toggleClass("tags__header_active")
+    $(this).parent().children(".tags__body").toggleClass("tags__body_active")
+})
+
+$("#application-btn").click(function(e) {
+    e.preventDefault()
+    $(".form").addClass("form_active")
+    $("body").addClass("lock")
+})
+
+$(window).click(function() {
+    $(".form").removeClass("form_active")
+    $("body").removeClass("lock")
+});
+$(document).keyup(function(e) {
+    if (e.key === "Escape") { 
+        $(".form").removeClass("form_active")
+        $("body").removeClass("lock")
+    }
+});
+$('.form__container, #application-btn').click(function(event){
+    event.stopPropagation();
+});
